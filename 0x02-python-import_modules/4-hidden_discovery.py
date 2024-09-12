8c6143f06a9fe66110adde8ca668e80356b36089
mabindajoash <mabindajoash@gmail.com>
2024-09-12 10:32:42 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden4
-    name = dir(hidden4)
+    import hidden_4
+    name = dir(hidden_4)
     for i in name:
         if i[:2] != "__":
             print(i)
