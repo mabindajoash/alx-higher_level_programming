6d90ff3c258ab788c4428e4c13b71ea227f8826d
mabindajoash <mabindajoash@gmail.com>
2024-09-12 10:06:42 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
         print("1 argument:")
         print(f"1: {argv[1]}")
     elif len(argv) < 2:
-        print("0 argument.")
+        print("0 arguments.")
     else:
-        print(f"{len(argv) - 1} arguments")
+        print(f"{len(argv) - 1} arguments:")
         for i in range(1, len(argv)):
             print(f"{i}: {argv[i]}")
