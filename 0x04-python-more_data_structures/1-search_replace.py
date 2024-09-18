1e28569e795b9162aec55f0e198d6371d5f19763
mabindajoash <joashmabinda@gmail.com>
2024-09-18 09:55:04 -0300

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    for i in new_list:
+        if search in new_list:
+            index = new_list.index(search)
+            new_list[index] = replace
+    return new_list
