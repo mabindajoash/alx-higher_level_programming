886c682f6f6d2e80b490931611be5dc08f3af932
mabindajoash <mabindajoash@gmail.com>
2024-10-16 03:21:30 -0700

    task 8

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,6 +7,7 @@ Module containing a class student
 class Student:
     """class defining a student"""
     def __init__(self, first_name, last_name, age):
+        """initializes student"""
         self.first_name = first_name
         self.last_name = last_name
 	self.age = age
