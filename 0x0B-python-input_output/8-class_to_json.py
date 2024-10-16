fa93c93da0e11bc6b7226b0afc8173b75f1b8d10
mabindajoash <mabindajoash@gmail.com>
2024-10-16 03:33:13 -0700

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,5 +5,7 @@
 
 
  def class_to_join(obj):
-    """returns a dictionary description"""
+     """returns the dictionary description with simple data structure
+        (list, dictionary, string, integer and boolean)
+	for JSON serialization of an object"""
     return obj.__dict__
