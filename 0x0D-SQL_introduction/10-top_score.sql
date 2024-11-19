6519cdd0c301fa81ea5d545ad6c52caa10f83067
mabindajoash <mabindajoash@gmail.com>
2024-11-19 10:02:38 -0800

    task 9

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- list records of the table
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- list all records with a name value
+SELECT score, name FROM second_table WHERE nmae != "" ORDER BY score DESC
