5511f17332b7f59a4a3169ebbc8dcde8a24c6cad
mabindajoash <mabindajoash@gmail.com>
2024-12-30 11:24:23 -0800

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module that takes in an argument and
+returns the state matching the argument"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s", (sys.argv[4],))
+    state_rows = cur.fetchall()
+    for row in state_rows:
+        print(row)
+    cur.close()
+    conn.close
