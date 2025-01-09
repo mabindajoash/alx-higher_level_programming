05c8095620b6f6fa75cf0f3ba58da80c71594a86
mabindajoash <mabindajoash@gmail.com>
2025-01-09 08:10:56 -0800

    task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
+"""
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
