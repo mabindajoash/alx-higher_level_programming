3805da0a9a1b363949ee1a6be49bcbda261f4313
mabindajoash <mabindajoash@gmail.com>
2025-01-17 10:03:00 -0800

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""POST an email and display body of the response
+"""
+import sys
+from urllib.request import Request, urlopen
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {'email' : sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode('ascii')
+    req = Request(url, data)
+    with urlopen(req) as response:
+        print(response.read().decode("utf-8"))
