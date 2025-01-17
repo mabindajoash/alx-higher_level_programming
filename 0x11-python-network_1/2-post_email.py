7f7c639c770b09772aa1bafe8c9ff25053910310
mabindajoash <mabindajoash@gmail.com>
2025-01-17 10:10:50 -0800

    task 3

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import urllib.parse
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {'email' : sys.argv[2]}
+    value = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(value).encode('ascii')
     req = Request(url, data)
     with urlopen(req) as response:
