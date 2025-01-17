646e64e8a2f421df67f15d3c4d55510a916612fb
mabindajoash <mabindajoash@gmail.com>
2025-01-17 09:08:48 -0800

    task 1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script to fetch resources from a url"""
+from urllib.request import Request, urlopen
+
+
+url = "https://alx-intranet.hbtn.io/status"
+req = Request(url)
+with urlopen(req) as response:
+    content = response.read()
+    print(f"Body response:")
+    print(f"    - type: {type(content)}")
+    print(f"    - content: {content}")
+    print(f"    - utf8 content: {content.decode('utf-8')}")
