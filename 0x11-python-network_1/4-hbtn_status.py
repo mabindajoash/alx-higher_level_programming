8ab6346589815292caf5029242cbb68851536a7e
mabindajoash <mabindajoash@gmail.com>
2025-01-18 02:10:02 -0800

    task 5

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,6 @@ import requests
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     r = requests.get(url)
-    r.encoding = 'utf-8'
-    print("Body response:$")
-    print("\t- type: {}$".format(type(r.text)))
-    print("\t- content: {}$".format(r.text))
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
